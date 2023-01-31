465a02d4b554be9e1e5a55dff6d63bf505ce205e
clivegi <123774280+clivegi@users.noreply.github.com>
2023-01-31 18:36:49 +0300

    Update bankamatik.py

--- bankamatik.py
+++ bankamatik.py
@@ -1,42 +1,51 @@
-hesapA={
-    "ad": "Sadık turan",
-    "hesapno": "123123",
-    "bakiye":3000,
-    "ekhesap":2000
-}
-
-hesapB={
-    "ad": "Ali turan",
-    "hesap no": "123123",
-    "bakiye":2000,
-    "ekhesap":1000
-}
-
-def paracek(hesap,miktar):
-    print(f"Merhaba {hesap['ad']}")
-    if hesap["bakiye"]>=miktar:
-        hesap["bakiye"]-=miktar
+parola="123456"
+hak=3
+while hak>0:
+    hak-=1
+    girilenparola=input("parolanızı giriniz: ")
+    if girilenparola==parola:
+        hesapA={
+            "ad":"Sadık Turan",
+            "hesapno":"123123",
+            "bakiye":3000,
+            "ekhesap":2000
+        }
+        hesapB={
+            "ad": "Ali turan",
+            "hesap no": "123123",
+            "bakiye":2000,
+            "ekhesap":1000
+        }
+        def paracek(hesap,miktar):
+            print(f"Merhaba {hesap['ad']}")
+            if hesap["bakiye"]>=miktar:
+                hesap["bakiye"]-=miktar
     
-        print("paranızı alabilirsinşz")
-    else:
-        toplam=hesap["bakiye"]+hesap["ekhesap"]
-        if(toplam>=miktar):
-            ekhesapkullanimi=input("ek hesap kullanılsın mı e/h")
-            if ekhesapkullanimi=="e":
-                ekhesapkullanimi=miktar-hesap["bakiye"]
-                hesap["bakiye"]=0
-                hesap["ekhesap"]-=ekhesapkullanimi
-                print("paranızı alabilirsiniz")
+                print("paranızı alabilirsinşz")
             else:
-                print(f"{hesap['hesapno']}nolu hesabınızda {hesap['bakiye']} bulunmaktadır")
-        else:
-            print("bakiye yetersiz")       
-def bakiyesorgu(hesap):
-    print(f"{hesap['hesapno']} nolu hesabınızda {hesap['bakiye']} Tl bulunmaktadır ek hesap limitiniz ise {hesap['ekhesap']} tl bulunmaktadır")
+                toplam=hesap["bakiye"]+hesap["ekhesap"]
+                if(toplam>=miktar):
+                    ekhesapkullanimi=input("ek hesap kullanılsın mı e/h")
+                    if ekhesapkullanimi=="e":
+                        ekhesapkullanimi=miktar-hesap["bakiye"]
+                        hesap["bakiye"]=0
+                        hesap["ekhesap"]-=ekhesapkullanimi
+                        print("paranızı alabilirsiniz")
+                    else:
+                        print(f"{hesap['hesapno']}nolu hesabınızda {hesap['bakiye']} bulunmaktadır")
+                else:
+                    print("bakiye yetersiz")       
+        def bakiyesorgu(hesap):
+            print(f"{hesap['hesapno']} nolu hesabınızda {hesap['bakiye']} Tl bulunmaktadır ek hesap limitiniz ise {hesap['ekhesap']} tl bulunmaktadır")
 
 
-paracek(hesapA,3000)
-bakiyesorgu(hesapA)
-print("*************")
-paracek(hesapA,2000)
-bakiyesorgu(hesapA)
+        paracek(hesapA,3000)
+        bakiyesorgu(hesapA)
+        print("*************")
+        paracek(hesapA,2000)
+        bakiyesorgu(hesapA)
+
+        
+    else:
+        print("parola yanlış tekrar deneyiniz")   
+    
